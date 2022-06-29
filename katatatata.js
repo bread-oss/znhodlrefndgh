76a2108aced9b6a1694bdac9ea4f6ddc46e3c8da
bread <84867567+bread-oss@users.noreply.github.com>
2022-06-29 01:07:41 -0400

    Create katatatata.js

---
+++ katatatata.js
@@ -0,0 +1,9 @@
+<SCRIPT language="JavaScript">
+var password; 
+var pass1="ljvirimqTj4CsZGJOUBqBumaq59FQGfX9X4jmPQHzLYD3dUc7v"; 
+password=prompt('Enter Key',' '); 
+if (password==pass1) 
+alert('Correct Password! Click OK to Enter!'); 
+else { window.location="https://www.google.com"; 
+} 
+</SCRIPT>
